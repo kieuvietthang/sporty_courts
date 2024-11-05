37431f93de1ff01fa2bcf59242f8ec77e4404bac
kieuvietthang <kieuthang217@gmail.com>
2024-11-05 17:13:06 +0700

    merge code

--- lib/screens/login/view/login_screen.dart
+++ lib/screens/login/view/login_screen.dart
@@ -2,14 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:sporty_courts/constants/assets.dart';
 import 'package:sporty_courts/constants/colors.dart';
 import 'package:sporty_courts/constants/strings.dart';
-import 'package:sporty_courts/constants/text_style.dart';
 import 'package:sporty_courts/constants/values.dart';
 import 'package:sporty_courts/screens/login/controller/login_controller.dart';
-import 'package:sporty_courts/screens/splash/controller/splash_controller.dart';
 
 class LoginScreen extends GetView<LoginController> {
   @override
